434a7cd54a49baab5701a70fabe2d010190d72c5
James Dai <james.dai@metadataconsulting.co.uk>
2019-04-17 14:22:43 +0100

    ClientGenderEtc, Gender, Ethnicity

--- src/main/groovy/uk/org/metadata/dhsc/lincolnshire/LincolnshireFakeData.groovy
+++ src/main/groovy/uk/org/metadata/dhsc/lincolnshire/LincolnshireFakeData.groovy
@@ -36,6 +36,12 @@ class LincolnshireFakeData {
 
 }
 
+/**
+ * Data to be extracted from a CSV table of (clientId, startDate, endDate)
+ *  * where clientId is a unique identifier of a row,
+ *  * where startDate is the earliest starting date of an event the client was involved in,
+ *  * and endDate is the latest end date of an event the client was involved in.
+ */
 class ClientStartEnd {
 
     @CsvBindByName(column = "ClientIdLocal", required = true)
@@ -60,5 +66,59 @@ class ClientStartEnd {
     @Override
     String toString() {
         return "ClientStartEnd[clientId: ${clientId}, startDate: ${startDate}, endDate: ${endDate}]"
+    }
+}
+
+/**
+ * (Fake) gender, ethnicity, date of birth, date of death for each client.
+ */
+class ClientGenderEtc {
+    Long clientId
+    Gender gender
+    Ethnicity ethnicity
+    Date dateOfBirth
+    Date dateOfDeath
+
+    static ClientGenderEtc from(ClientStartEnd clientStartEnd) {
+
+    }
+}
+
+enum Gender {
+    MALE("Male"),
+    FEMALE("Female"),
+    NOT_KNOWN("Not Known")
+
+    private String name
+
+    Gender(String name) {
+        this.name = name
+    }
+}
+
+enum Ethnicity {
+    White_English_Welsh_Scottish_Northern_Irish_British("White (English / Welsh / Scottish / Northern Irish / British)"),
+    White_Irish("White Irish"),
+    White_Gypsy_or_Irish_Traveller("White (Gypsy or Irish Traveller)"),
+    Any_other_White_background("Any other White background"),
+    White_and_Black_Caribbean("White and Black Caribbean"),
+    White_and_Black_African("White and Black African"),
+    White_and_Asian("White and Asian"),
+    Any_other_mixed_multiple_ethnic_background("Any other mixed / multiple ethnic background"),
+    Indian("Indian"),
+    Pakistani("Pakistani"),
+    Bangladeshi("Bangladeshi"),
+    Chinese("Chinese"),
+    Any_other_Asian_background("Any other Asian background"),
+    African("African"),
+    Caribbean("Caribbean"),
+    Any_other_Black_African_Caribbean_background("Any other Black / African / Caribbean background"),
+    Arab("Arab"),
+    Any_other_ethnic_group("Any other ethnic group"),
+    Refused("Refused"),
+    Undeclared_Not_known("Undeclared / Not known"),
+    private String name
+    Ethnicity(String name) {
+        this.name = name
     }
 }
